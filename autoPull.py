d7bd332379a5f1bf0d3bfc48710057924fd753e9
cfacon <cedrick.facon@gmail.com>
2019-03-31 20:26:32 +0100

    autoPull start

---
+++ autoPull.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+import server as srv
+#import robotv1.server as srv
+
+#os.chdir("robotv1")
+
+def git(*args):
+    return subprocess.check_call(['git'] + list(args))
+
+git("remote", "set-url", "origin", "https://cfacon:git1Secret,42@github.com/cfacon/robotv1.git")
+git("pull")
+
+srv.start()
--- globalConfig.py
+++ globalConfig.py
@@ -1,3 +1,14 @@
+#GPIO rpi
+
+moteur1_enA = 16 #36
+moteur1_en1 = 20 #38
+moteur1_en2 = 21 #40
+
+moteur2_enB = 13 #33
+moteur2_en3 = 19 #35
+moteur2_en4 = 26 #37
+
+
 # Javascript related
 maxJoystickValue = 100 # Same value as in views/gamepad.tpl 
 leftJoystickX = 0 # Unused 
